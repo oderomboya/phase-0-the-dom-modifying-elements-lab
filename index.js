a7edd23ae3c03c9fef8f6e525a0a00def6dcb0f4
oderomboya <davismboya3@gmail.com>
2023-03-13 07:21:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+
+main.remove();
+let newHeader =document.createElement('h1');
+newHeader.id='victory'
+newHeader.innerHTML="Alvin is the champion";
+
+
+// function edit() {
+//   main.remove();
+
+//   let newHeader = document.createElement("h1");
+//   newHeader.id = "victory";
+//   newHeader.innerHTML = "Alvin is the champion";
+//   document.body.appendChild(newHeader);
+// }
+
+// edit();
